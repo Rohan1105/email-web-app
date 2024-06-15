d0388a7c1ccdb7967b95e432c0dc0ba8503e5f8e
Prince Roy <princeku.ray143@gmail.com>
2024-06-15 16:56:00 +0530

    create profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
+  isClicked: boolean = false;
   constructor() {}
+
+  handleClick() {
+    this.isClicked = !this.isClicked;
+  }
+
+  handleLogout() {}
 }
